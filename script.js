6895f5c58c7e24deb702b0f3f5bc3ea1541060b0
priscillavassallo <priscillavassallo96@gmail.com>
2025-11-20 23:28:57 -0300

    acende a próxima cor

--- script.js
+++ script.js
@@ -23,3 +23,13 @@ let shuffleOrder = () => {
     }
 }
 
+let lightColor = (element, number) => {
+    number = number * 500;
+    setTimeout(() => {
+        element.classList.add('selected');
+    }, number - 250);
+    setTimeout(() => {
+        element.classList.remove('selected');
+    });
+}
+
