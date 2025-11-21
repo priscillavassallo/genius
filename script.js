c2fecfbe85460f8ba51e9a8f2d26d746c7247a5f
priscillavassallo <priscillavassallo96@gmail.com>
2025-11-20 22:18:10 -0300

    estiliza containers

---
+++ script.js
@@ -0,0 +1,14 @@
+let order = [];
+let clickedOrder = [];
+let score = 0;
+
+//0 - verde
+//1 - vermelho
+//2 - amarelo
+//3 - azul
+
+const verde = document.querySelector('.verde');
+const vermelho = document.querySelector('.vermelho');
+const amarelo = document.querySelector('.amarelo');
+const azul = document.querySelector('.azul');
+
